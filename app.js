94bd3ab8997a81900ab56b70715a63501301e59d
thenameisvk <vk920373@gmail.com>
2025-09-29 16:59:46 +0530

    Update app.js
    Changed  to default port

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
